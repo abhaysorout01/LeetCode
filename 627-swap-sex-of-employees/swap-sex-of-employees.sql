66864a81babb60286d99b151a1679959b37db290
Abhay Sorout <abhaysorout02@gmail.com>
2026-09-06 13:41:44 +0530

    Time: 279 ms (45.03%) | Memory: 0B (100.00%) - LeetSync

--- 627-swap-sex-of-employees/swap-sex-of-employees.sql
+++ 627-swap-sex-of-employees/swap-sex-of-employees.sql
@@ -1,6 +1,2 @@
 # Write your MySQL query statement below
-UPDATE Salary
-SET sex = CASE
-    WHEN sex = 'm' THEN 'f'
-    ELSE 'm'
-END;
+UPDATE Salary SET sex = CASE WHEN sex = 'm' THEN 'f' ELSE 'm' END;
