ae09ae1d2879cc2f20d46f2658dd133e4bb24eb3
Abhay Sorout <abhaysorout02@gmail.com>
2026-09-06 13:27:34 +0530

    Time: 1 ms (100.00%) | Memory: 47.5 MB (12.39%) - LeetSync

--- 2903-insert-greatest-common-divisors-in-linked-list/insert-greatest-common-divisors-in-linked-list.java
+++ 2903-insert-greatest-common-divisors-in-linked-list/insert-greatest-common-divisors-in-linked-list.java
@@ -1,13 +1,3 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     private int gcd(int a, int b) {
         while(b != 0) {
@@ -18,19 +8,14 @@ class Solution {
         return a;
     }
     public ListNode insertGreatestCommonDivisors(ListNode head) {
-        if(head == null || head.next == null) return head;
-        ListNode dummy = new ListNode(0);
-        ListNode ref = dummy;
-        while(head.next != null) {
-            ref.next = new ListNode(head.val);
-            int a = head.val;
-            ref = ref.next;
-            int b = head.next.val;
-            ref.next = new ListNode(gcd(a,b));
-            ref = ref.next;
-            head = head.next;
+        ListNode cur = head;
+        while(cur != null && cur.next != null) {
+            int g = gcd(cur.val, cur.next.val);
+            ListNode node = new ListNode(g);
+            node.next = cur.next;
+            cur.next = node;
+            cur = node.next;
         }
-        ref.next = new ListNode(head.val);
-        return dummy.next;
+        return head;
     }
 }
