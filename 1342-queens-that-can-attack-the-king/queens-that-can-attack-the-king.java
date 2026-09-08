22ed9e9cb1ce5285c1a25ec573c31b2211482d83
Abhay Sorout <abhaysorout02@gmail.com>
2026-09-08 20:51:46 +0530

    Time: 1 ms (81.35%) | Memory: 44.1 MB (86.90%) - LeetSync

---
+++ 1342-queens-that-can-attack-the-king/queens-that-can-attack-the-king.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<List<Integer>> queensAttacktheKing(int[][] queens, int[] king) {
+        List<List<Integer>> ans=new ArrayList<>();
+        boolean[][] matrix = new boolean[8][8];
+        for(int i[]: queens) matrix[i[0]][i[1]]=true;
+
+        int[][] dir={{-1,0},{1,0},{0,1},{0,-1},{-1,-1},{-1,1},{1,-1},{1,1}};
+        for(int d[]: dir){
+            int dr = king[0];
+            int dc = king[1];
+            while(dr >= 0 && dr < 8 && dc >= 0 && dc < 8){
+                dr += d[0];
+                dc += d[1];
+                if (dr < 0 || dr >= 8 || dc < 0 || dc >= 8) break;
+                if(matrix[dr][dc]){
+                    ans.add(Arrays.asList(dr,dc));
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+}
